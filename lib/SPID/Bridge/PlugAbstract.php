a459f120a23adbdf8e0627c7e21f061e1e7372c0
karim cheratt <karim.cheratt@yahoo.ca>
2016-04-15 00:41:00 -0400

    rimka fix

--- lib/SPID/Bridge/PlugAbstract.php
+++ lib/SPID/Bridge/PlugAbstract.php
@@ -1,26 +1,18 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
  * @author karim cheratt
  */
 namespace SPID\Bridge;
 
 abstract class PlugAbstract
-{    
+{
     private $driver;
-    
+
     protected $queriesTable = [];
-    
+
     // $queriesParam = [string $query][mixed $value]
     protected $queriesParam = [];
-    
+
     // $queriesParam = ['status'=> int Code , 'data' => mixed]
     protected $result = array();
 
@@ -28,64 +20,64 @@ abstract class PlugAbstract
     {
         $this->driver = $driver;
     }
-    
+
     /******************************************
                     * PRIVATE *
     *******************************************/
-    
+
     private function queryID($query)
     {
         return md5($query);
     }
-    
+
     /******************************************
                     * PUBLIC *
     *******************************************/
-    
+
     public function getStatus()
     {
         return $this->driver->status();
     }
-    
+
     public function errorMessage()
     {
         return $this->driver->error_message();
     }
-    
+
     protected function executeTransaction()
     {
         $this->result = null;
-        $this->driver->open_connection();        
+        $this->driver->open_connection();
         $this->driver->start_transaction();
-        
+
         $commit = true;
         // sort queriesTable asc
         foreach ($this->queriesTable as $query) {
-            
+
             $this->runQuery($query);
-            
-            if($this->isError()){                
+
+            if($this->isError()){
                 $this->driver->cancel_transaction();
                 $commit = false;
                 break;
             }
-        }        
+        }
         if($commit){
             $this->driver->commit_transaction();
         }
-        
+
         $this->resetQueries();
     }
-    
+
     private function runQuery($query)
     {
         if(!empty($this->queriesParam[$query])){
-            
+
             foreach($this->queriesParam[$query] as $param){
-                
+
                 $this->driver->execute_query($query , $param);
                 $this->result[] = $this->driver->result();
-                        
+
                 if($this->isError()){ break;}
             }
         }
@@ -93,13 +85,13 @@ abstract class PlugAbstract
             $this->driver->execute_query();
         }
     }
-    
+
     protected function resetQueries()
     {
         $this->queriesTable = [];
         $this->queriesParam = [];
     }
-    
+
     protected function setQuery($query , $param = null)
     {
         if(!in_array($query , $this->queriesTable)){
@@ -109,12 +101,12 @@ abstract class PlugAbstract
            $this->queriesParam[$query][] = $param;
         }
     }
-    
-    
+
+
     protected function isError()
     {
         return ( 500 === $this->driver->status() );
     }
-    
-    
+
+
 }
