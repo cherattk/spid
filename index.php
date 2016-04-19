adc4db65923d8e562faefb35eec01df719a529fb
cheratt karim <cheratt.karim.dev@gmail.com>
2016-04-19 22:52:14 +0000

    fix

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ $serviceConfig = array(
         'driver' => '\SPID\Driver\PDODriver',
         'driver.config' => array(
             // required for PDODriver
-            'dsn' => 'mysql:dbname=c9;host=127.0.0.1;port=3306;',
+            'dsn' => 'mysql:dbname=dbname;host=hostname;port=port;',
             
             //optional for some database
-            'db.user' => 'cherattk',
+            'db.user' => '',
             'db.pass' => ''
         )
     )
